d68017e47b13bcc8eea12c6e4d30d908572ac9f0
Matheus Sousa Dias <75400361+L4TN@users.noreply.github.com>
2023-06-11 21:37:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 import json
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": ["https://adugo-game-frontend-01.onrender.com", "https://adugo-game-frontend-prd.onrender.com","http://127.0.0.1:5500"]}})
+CORS(app, resources={r"/api/*": {"origins": ["https://adugo-game-frontend-01.onrender.com",
+                                             "https://adugo-game-frontend-prd.onrender.com", "http://127.0.0.1:5500"]}})
+
+database = 'JogoOnca'
+user = 'MasterOnca'
+password = 'onca1020'
+host = 'jogodaonca.cveloztfcqty.us-east-1.rds.amazonaws.com'
+port = '5432'
+
+database_url = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+
+engine = create_engine(database_url)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class Jogador(Base):
+    __tablename__ = 'jogador'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    isactive = Column(Boolean, default=True)
+    login = Column(String)
+    senha = Column(String)
+    email = Column(String)
+
+
+Base.metadata.create_all(engine)
+
 
 class Cell:
     def __init__(self, classList, x, y):
@@ -12,8 +46,39 @@ class Cell:
         self.x = x
         self.y = y
 
+
 last_move = []
 file_path = "last_move.txt"
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    login = request.args.get('Login')
+    senha = request.args.get('Senha')
+
+    user = session.query(Jogador).filter_by(login=login, senha=senha).first()
+
+    if user:
+        # Incluir o ID do usuário no JSON de resposta
+        return jsonify(status="success", user_id=user.id), 200
+    else:
+        return jsonify(status="fail"), 401
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    nome = request.args.get('Nome')
+    login = request.args.get('Login')
+    senha = request.args.get('Senha')
+    email = request.args.get('Email')
+
+    novo_jogador = Jogador(nome=nome, isactive=True,
+                           login=login, senha=senha, email=email)
+    session.add(novo_jogador)
+    session.commit()
+
+    return jsonify(status="success"), 201
+
 
 @app.route('/api/send-move', methods=['POST'])
 def send_move():
@@ -24,6 +89,7 @@ def send_move():
 
     return "", 200
 
+
 @app.route('/api/check', methods=['GET'])
 def get_move():
     global last_move
@@ -32,10 +98,12 @@ def get_move():
     if len(last_move) == 0:
         return jsonify(status="no move"), 200
 
-    response = jsonify(status="move available", move=[cell.__dict__ for cell in last_move])
+    response = jsonify(status="move available", move=[
+                       cell.__dict__ for cell in last_move])
     last_move = []
-    
+
     return response, 200
+
 
 def save_to_file(move):
     try:
@@ -44,12 +112,13 @@ def save_to_file(move):
     except Exception as e:
         print(f"Error writing to file: {e}")
 
+
 def load_from_file():
     global last_move
     if not os.path.isfile(file_path):
         print("File does not exist. No move available.")
         return
-    
+
     try:
         with open(file_path, 'r') as file:
             data = file.read()
@@ -60,12 +129,6 @@ def load_from_file():
     except Exception as e:
         print(f"Error reading from file: {e}")
 
-    
-    try:
-        with open(file_path, 'r') as file:
-            last_move = [Cell(**cell) for cell in json.load(file)]
-    except Exception as e:
-        print(f"Error reading from file: {e}")
 
 if __name__ == "__main__":
     app.run(port=5003)
